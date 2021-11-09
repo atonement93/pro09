8605e25dc9e193ba7eb75120237f2896d7f8940d
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-11-09 20:11:01 +0900

    웹페이지 연동

--- src/main/java/sec01/ex02/LoginServlet.java
+++ src/main/java/sec01/ex02/LoginServlet.java
@@ -1,7 +1,8 @@
-package sec01.ex01;
+package sec01.ex02;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,14 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-//@WebServlet("/login")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
     /**
-     * Default constructor. 
+     * @see HttpServlet#HttpServlet()
      */
     public LoginServlet() {
+        super();
         // TODO Auto-generated constructor stub
     }
 
@@ -55,8 +57,12 @@ public class LoginServlet extends HttpServlet {
 		data+="이메일 : "+user_email;
 		data+="<br>";
 		data+="휴대전화 : "+user_hp;
-		data+="</body></html>";
+		
 		out.print(data);
+		
+		user_address=URLEncoder.encode(user_address,"utf-8");
+		out.print("<a href='/pro09/second?user_id="+user_id+"&user_pw="+user_pw+"&user_address="+user_address+"'><br>두번째 서블릿으로 보내기</a>");
+		data+="</body></html>";
 	}
 
 }
