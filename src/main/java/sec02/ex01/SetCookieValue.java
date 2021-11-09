9b180d40473ceaaae2d44851472f056af97466b6
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-11-09 20:26:48 +0900

    서블릿 쿠키

---
+++ src/main/java/sec02/ex01/SetCookieValue.java
@@ -0,0 +1,51 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SetCookieValue
+ */
+@WebServlet("/set")
+public class SetCookieValue extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SetCookieValue() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		Cookie cookie = new Cookie("cookieTest",URLEncoder.encode("JSP프로그래밍","utf-8"));
+		cookie.setMaxAge(24*60*60);
+		response.addCookie(cookie);
+		out.println("현재시간으로 cookie를 저장합니다.");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
